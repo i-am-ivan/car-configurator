614402d487cbe6c4667a8bb63a57cfc0fa3d6328
i-am-ivan <navicorpdesigns@gmail.com>
2022-11-26 13:49:36 +0300

    1.0.1

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const sql = require('mssql');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// DB connection string
+var dbConnection = {
+    user: "...",
+    password: "...",
+    server: "l192.168.100.102",
+    driver: "msnnodesql",
+    database: "cconfigurator",
+    connectionString: ""
+};
 
 // Parsing middleware
 // Parse application
@@ -26,6 +37,16 @@ app.set('view engine', 'hbs');
 app.get('', (req, res) => {
     res.render('home')
 });
+
+// Render Summary page
+app.get('/configurators', (req, res) => {
+    res.render('configurators')
+})
+
+// Render Edit page
+app.get('/edit', (req, res) => {
+    res.render('edit')
+})
 
 // Deploy server
 app.listen(port, () => console.log(`\n Listening on port ${port}`));
